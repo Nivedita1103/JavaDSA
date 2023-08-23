8574f1332f5fcac7741c6593b1adc682033cef89
Nivedita <nskokil@gmail.com>
2023-08-23 20:12:20 +0530

    code for the day

---
+++ binarypattern.java
@@ -0,0 +1,8 @@
+public class binarypattern {
+    public static void main(String[] args) {
+        int n=7;
+        for(int i=0; i<n; i++){
+            System.out.println(Integer.toString(i));
+        }
+    }
+}
